3103e94b8ba643e26a5cd937102510183ed36c60
bryahchin.mihail <bryahchin.mihail@otr.ru>
2022-01-24 20:22:05 +0300

    DZ18 hystrix

--- src/main/java/otus/orm/exp/service/GenreServiceImpl.java
+++ src/main/java/otus/orm/exp/service/GenreServiceImpl.java
@@ -1,14 +1,13 @@
 package otus.orm.exp.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import otus.orm.exp.repository.GenresRepository;
 import otus.orm.exp.entity.Genre;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class GenreServiceImpl implements GenresService {
@@ -20,6 +19,7 @@ public class GenreServiceImpl implements GenresService {
     }
 
     @Override
+    @HystrixCommand(commandKey = "genres", fallbackMethod = "findDefaultGenre")
     public List<Genre> getAllGenres() {
         List<Genre> genres = repository.findAll();
         return CollectionUtils.isEmpty(genres) ? new ArrayList<>() : genres;
@@ -40,5 +40,12 @@ public class GenreServiceImpl implements GenresService {
     @Transactional
     public Optional<Genre> saveGenre(Genre genre) {
         return Optional.of(repository.save(genre));
+    }
+
+    public  List<Genre> findDefaultGenre() {
+        Genre genre = new Genre();
+        genre.setId(1L);
+        genre.setGenre("Default genre book");
+        return Collections.singletonList(genre);
     }
 }
